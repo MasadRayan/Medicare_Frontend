123541c1a9bed7604e0ed48beb9469b4f32960d6
Masad Rayan <masadrayan2002@gmail.com>
2026-09-25 01:55:40 +0600

    Add Providers and QueryProvider components for React Query integration

---
+++ src/providers/query.provider.tsx
@@ -0,0 +1,36 @@
+import { environmentManager, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+function makeQueryClient() {
+    return new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 1000 * 60, // 1 minute
+            }
+        }
+    })
+}
+
+let browserQueryClient : QueryClient | undefined = undefined
+
+function getQueryClient() {
+    if (environmentManager.isServer()) {
+        return makeQueryClient()
+    } else {
+        if (!browserQueryClient) {
+            browserQueryClient = makeQueryClient()
+        }
+        return browserQueryClient
+    }
+}
+
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+    const queryClient = getQueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider
