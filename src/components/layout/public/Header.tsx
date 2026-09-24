0ac09563895591d1881a409a0b378bd8128c5bf9
Masad Rayan <masadrayan2002@gmail.com>
2026-09-25 03:30:53 +0600

    feat: add login form component with validation and UI elements
    - Implemented a new LoginForm component using React and TanStack Form.
    - Created reusable UI components for fields, labels, inputs, and separators.
    - Added validation schema for login using Zod.
    - Included a password visibility toggle in the login form.
    - Introduced a new image asset for registration.

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
@@ -8,15 +9,26 @@ const Header = () => {
   ];
 
   return (
-    <header className="w-full h-16 border-b flex items-center justify-center gap-4">
-      <nav className="flex gap-5">
-        {routes.map((route) => (
-          <Link key={route.url} href={route.url}>
-            {" "}
-            {route.name}
-          </Link>
-        ))}
-      </nav>
+    <header className="w-full h-16 border border-b">
+      <div className="flex justify-between items-center h-full max-w-7xl mx-auto">
+        <div>MediCare</div>
+        <nav className="flex gap-5">
+          {routes.map((route) => (
+            <Link key={route.url} href={route.url}>
+              {route.name}
+            </Link>
+          ))}
+        </nav>
+        <div>
+          <Button
+            variant="outline"
+            render={<Link href="/login">Login</Link>}
+            nativeButton={false}
+          >
+            login
+          </Button>
+        </div>
+      </div>
     </header>
   );
 };
